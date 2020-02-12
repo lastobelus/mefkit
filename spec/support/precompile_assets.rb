89a21891d745a99e6fb7201b589512cd1bc326ab
Michael Johnston <lastobelus@mac.com>
2020-02-12 12:35:01 -0800

    switch assets:precompile to webpacker:compile in spec/suppport

--- spec/support/precompile_assets.rb
+++ spec/support/precompile_assets.rb
@@ -1,11 +1,11 @@
 RSpec.configure do |config|
   config.before :all do
-    unless File.exist?(Rails.root.join("public/test-assets"))
+    unless File.exist?(Rails.root.join("public/packs-test"))
       case self.class.metadata[:type]
       when :feature, :view, :request
         STDOUT.write "Precompiling assets..."
 
-        system "bundle exec rake assets:precompile > /dev/null 2>&1"
+        system "bundle exec rake webpacker:compile > /dev/null 2>&1"
 
         STDOUT.puts "\n Done."
       end
