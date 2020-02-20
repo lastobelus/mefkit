4dc52eff53a5ede7e9a16ab1b73e90e67352720e
Michael Johnston <lastobelus@mac.com>
2020-02-19 20:59:26 -0800

    actioncable roundtrip with cypress test
    using stimulus

--- app/channels/ping_channel.rb
+++ app/channels/ping_channel.rb
@@ -1,7 +1,15 @@
 class PingChannel < ApplicationCable::Channel
+  STREAM = "mefkit_ping_channel"
+
   def subscribed
     Rails.logger.debug { "PingChannel subscribed" }
-    stream_from "mefkit_ping_channel"
+    stream_from STREAM
+    ensure_confirmation_sent
+    transmit(
+      ping: {
+        msg: "subscribed",
+      },
+    )
   end
 
   def unsubscribed
@@ -9,10 +17,23 @@ class PingChannel < ApplicationCable::Channel
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def pong(msg)
-    Rails.logger.info { ">>>>>> PingChannel pong: #{msg}" }
+  def pong(data)
+    Rails.logger.info { ">>>>>> PingChannel pong: #{data}" }
+    transmit(
+      ping: {
+        msg: "ACK: #{data["msg"]}",
+      },
+    )
   end
 
   class << self
+    def ping(msg)
+      ActionCable.server.broadcast(
+        STREAM,
+        ping: {
+          msg: msg,
+        },
+      )
+    end
   end
 end
--- app/javascript/channels/ping_channel.js
+++ app/javascript/channels/ping_channel.js
@@ -1,6 +1,8 @@
 import consumer from "./consumer";
 
-consumer.subscriptions.create("PingChannel", {
+let callbacks = new Set();
+
+const pingChannel = consumer.subscriptions.create("PingChannel", {
   connected() {
     // Called when the subscription is ready for use on the server
     console.log("PingChannel connected");
@@ -14,5 +16,18 @@ consumer.subscriptions.create("PingChannel", {
   received(data) {
     // Called when there's incoming data on the websocket for this channel
     console.log("PingChannel received", data);
+    for (let callback of callbacks) {
+      callback.call(null, data.ping.msg);
+    }
   }
 });
+
+const pong = msg => {
+  pingChannel.perform("pong", { msg });
+};
+
+const receiveMessage = fn => {
+  callbacks.add(fn);
+};
+
+export { pong, receiveMessage };
